edc63c245775fcc49256e783f3f8bd0063b3245e
Arnaiz23 <aaf110501@gmail.com>
2024-07-06 22:51:41 +0200

    feat ✨: Create the controls for start and stop

---
+++ src/components/Controls.tsx
@@ -0,0 +1,25 @@
+import { store } from "../store";
+import { OPTIONS } from "../consts";
+
+export default function Controls() {
+  const { counterState, changeCounterState } = store();
+
+  const handleClick = () => {
+    if (counterState === OPTIONS.START) {
+      changeCounterState(OPTIONS.STOP);
+    } else {
+      changeCounterState(OPTIONS.START);
+    }
+  };
+
+  return (
+    <section className="w-full h-full grid place-items-center pb-4">
+      <button
+        onClick={handleClick}
+        className="capitalize p-5 rounded-full border border-white aspect-square text-xl font-bold"
+      >
+        {counterState}
+      </button>
+    </section>
+  );
+}
