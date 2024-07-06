76d63391fd008dc817ea80b986cef20f885681fa
Arnaiz23 <aaf110501@gmail.com>
2024-07-06 22:50:20 +0200

    feat 🎉: Create the project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+import Header from "./components/Header";
+import Count from "./components/Count";
+import Controls from "./components/Controls";
+
+function App() {
+  return (
+    <div
+      className="dark:bg-[#2f2f2f] dark:text-white w-full min-h-dvh grid"
+      style={{ gridTemplateRows: "96px 1fr 150px" }}
+    >
+      <Header />
+      <Count />
+      <Controls />
+    </div>
+  );
+}
+
+export default App;
