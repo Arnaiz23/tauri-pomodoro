928bd95cee86ce8d81e03a68dca3993ea8be2f3d
Arnaiz23 <aaf110501@gmail.com>
2024-07-07 15:25:11 +0200

    fix 🐛: Change the counters durations

--- src/consts.ts
+++ src/consts.ts
@@ -1,11 +1,8 @@
 export const COUNTERS = {
-  POMODORO: 2,
-  BREAK: 1,
-  LONG_BREAK: 3,
+  POMODORO: 1200,
+  BREAK: 300,
+  LONG_BREAK: 900,
 };
-// POMODORO: 1200,
-// BREAK: 300,
-// LONG_BREAK: 900
 
 export const OPTIONS = {
   START: "start",
